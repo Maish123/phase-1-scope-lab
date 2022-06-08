8f4b45d9b010ad36019f649e072f99ab14b1c6e7
Denis Maina <Denis.maina@student.moringaschool.com>
2022-06-09 00:04:09 +0300

    updated file

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = ("Bob");
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase;
+}
+upperCaseCustomerName();
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+    return bestCustomer;
+} 
+
+setBestCustomer();
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+
+overwriteBestCustomer();
+
+const leastFavoriteCustomer = 'Jano';
+
+function changeLeastFavoriteCustomer(){
+    const  leastFavoriteCustomer = 'Deno';
+    return leastFavoriteCustomer;
+}
+
+changeLeastFavoriteCustomer();
