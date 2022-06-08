7905ec70db1e880683aa026ce889011366d51314
Denis Maina <Denis.maina@student.moringaschool.com>
2022-06-08 23:58:07 +0300

    commit tree 276eb807a43ba9c93de505d0ba968a6e43b96204

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = ("Name");
+
+function upperCaseCustomerName(){
+    return customerName.toLocaleUpperCase;
+}
+upperCaseCustomerName();
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+    return bestCustomer;
+} 
+
+setBestCustomer();
+
+function overwriteBestCustomer(){
+    bestCustomer = "Yes bob";
+    return bestCustomer;
+}
+
+overwriteBestCustomer();
+
+const leastFavoriteCustomer = "xx";
+
+function changeLeastFavoriteCustomer(){
+    const  leastFavoriteCustomer = "yy";
+    return leastFavoriteCustomer;
+}
+
+changeLeastFavoriteCustomer();
